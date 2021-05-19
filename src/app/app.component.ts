2e9f252f9cdfb750be007e748ee96947edb7f6ce
gordongest <gordon.gest@gmail.com>
2021-05-19 10:36:06 -0600

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,69 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+  length = 0;
+
+  onLengthChange(val: string): void {
+    this.length = parseInt(val);
+  }
+
+  onButtonClick() {
+    console.log(`
+      Preparing to generate a password with the following criteria:
+      Includes Letters: ${this.includeLetters}
+      Includes Numbers: ${this.includeNumbers}
+      Includes Symbols: ${this.includeSymbols}
+      Length: ${this.length}
+    `);
+
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()-+';
+
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generated = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const rand = Math.floor(Math.random() * validChars.length);
+      generated += validChars[rand];
+    }
+
+    this.password = generated;
+  }
+
+  onLettersClick() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onNumbersClick() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onSymbolsClick() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  getPassword() {
+    return this.password;
+  }
 }
